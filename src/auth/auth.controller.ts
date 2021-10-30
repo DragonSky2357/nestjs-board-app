fbcaf316c92ec7fe844c85d28486af7a32374b60
unknown <qkrdydals327@gmail.com>
2021-10-31 04:31:31 +0900

    create 2021 10 31

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,37 @@
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { AuthService } from './auth.service';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(
+    @Body(ValidationPipe) authcredentialsDto: AuthCredentialsDto,
+  ): Promise<void> {
+    return this.authService.signUp(authcredentialsDto);
+  }
+
+  @Post('/signin')
+  sigin(
+    @Body(ValidationPipe) authcredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authcredentialsDto);
+  }
+
+  @Post('/test')
+  @UseGuards(AuthGuard())
+  test(@GetUser() user: User) {
+    console.log('user', user);
+  }
+}
